2b5c1fc135bbdcfdd4b4f008a79f4d9f7aac89a3
mloche <68994057+mloche@users.noreply.github.com>
2021-03-09 20:30:26 +0100

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -2,7 +2,7 @@
 
 apt install apache2 mariadb-server gnupg lsb-release apt-transport-https ca-certificates
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/source.list.d/php.list
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
 apt update
 apt install php7.4 php7.4-mysql
 wget -O /tmp/wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
