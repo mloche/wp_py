b36d5ea5a2a6e054e2d435a5574f9ab89588119e
mloche <68994057+mloche@users.noreply.github.com>
2021-03-15 12:45:49 +0100

    Update save.py

--- save.py
+++ save.py
@@ -9,6 +9,7 @@ sys.path.append('./modules/')
 import yaml
 import os
 import shutil
+import stats
 import subprocess
 import fileinput
 import logging
@@ -41,6 +42,31 @@ def aws_save(yaml_data):
 	print("backup AWS in folder ",backup_folder, "in bucket", bucket_name)
 
 
+def copytree(src, dst, symlinks = False, ignore = None):
+	if not os.path.exists(dst):
+		os.makedirs(dst)
+		shutil.copystat(src, dst)
+	lst = os.listdir(src)
+	if ignore:
+		excl = ignore(src, lst)
+		lst = [x for x in lst if x not in excl]
+	for item in lst:
+		s = os.path.join(src, item)
+		d = os.path.join(dst, item)
+		if symlinks and os.path.islink(s):
+			if os.path.lexists(d):
+				os.remove(d)
+			os.symlink(os.readlink(s), d)
+			try:
+				st = os.lstat(s)
+				mode = stat.S_IMODE(st.st_mode)
+				os.lchmod(d, mode)
+			except:
+				pass # lchmod not available
+		elif os.path.isdir(s):
+			copytree(s, d, symlinks, ignore)
+		else:
+			shutil.copy2(s, d)
 
 
 
